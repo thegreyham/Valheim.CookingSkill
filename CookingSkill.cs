68608040bc4c9f1bf286392e220a9616fe9aa235
thegreyham <gmc0rm1ck87@gmail.com>
2021-03-14 00:17:21 +0000

    Initial Cooking Skill

--- CookingSkill.cs
+++ CookingSkill.cs
@@ -22,12 +22,62 @@ namespace CookingSkill
         public static ConfigEntry<int> nexusID;
         public static ConfigEntry<bool> modEnabled;
 
-        const int SKILL_TYPE = 745;
+        const int COOKING_SKILL_ID = 483;  //nexus mod id :)
 
-        void Awake()
+        private void Awake()
         {
-            SkillInjector.RegisterNewSkill(SKILL_TYPE, "Cooking", "Improves Cooked Food", 1.0f, null, Skills.SkillType.Unarmed);
-        }
-    ﻿}
+            nexusID = Config.Bind<int>("General", "NexusID", 483, "NexusMods ID for updates.");
+            modEnabled = Config.Bind<bool>("General", "Enabled", true, "Enable the mod.");
 
+            if (!modEnabled.Value)
+                return;
+
+            harmony = new Harmony(PluginGUID);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            try
+            {
+                SkillInjector.RegisterNewSkill(COOKING_SKILL_ID, "Cooking", "Improves Cooked Food", 1.0f, null, Skills.SkillType.Unarmed);
+            }
+            catch { }
+        }
+
+        private void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+
+        // increase cooking skill when placing an item on the cooking station
+        [HarmonyPatch(typeof(CookingStation), "CookItem")]
+        internal class Patch_CookingStation_CookItem
+        {
+            static void Postfix(ref bool __result)
+            {
+                if (__result)
+                    Player.m_localPlayer.RaiseSkill((Skills.SkillType)COOKING_SKILL_ID, 0.25f);
+            }
+        }
+
+        // increase cooking skill when removing a successful cooked item from cooking station
+        [HarmonyPatch(typeof(CookingStation), "RPC_RemoveDoneItem")]
+        internal class Patch_CookingStation_RPC_RemoveDoneItem
+        {
+            static void Prefix(ref CookingStation __instance, ref ZNetView ___m_nview)
+            {
+                Traverse t_cookingStation = Traverse.Create(__instance);
+                ZDO zdo = ___m_nview.GetZDO();
+                for (int slot = 0; slot < __instance.m_slots.Length; ++slot)
+                {
+                    string itemName = zdo.GetString(nameof(slot) + slot);
+                    bool isItemDone = t_cookingStation.Method("IsItemDone", itemName).GetValue<bool>();
+                    if (itemName != "" && itemName != __instance.m_overCookedItem.name && isItemDone)
+                    {
+                        Player.m_localPlayer.RaiseSkill((Skills.SkillType)COOKING_SKILL_ID, 0.75f);
+                        break;
+                    } 
+                }
+            }
+        }
+        // float skillLevel = Player.m_localPlayer.GetSkillFactor((Skills.SkillType)COOKING_SKILL_ID)
+    }
 }
